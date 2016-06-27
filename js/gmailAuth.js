d17f5d5d0bcb16ba8c115ff41dc5319a2b17e3ff
dannodan89 <dannodan89@gmail.com>
2016-06-27 15:52:40 -0400

    final commit

--- js/gmailAuth.js
+++ js/gmailAuth.js
@@ -1,118 +1,22 @@
-      // Your Client ID can be retrieved from your project in the Google
-      // Developer Console, https://console.developers.google.com
-      var CLIENT_ID = '70783372561-sj6jonf3h6mp73i75o2u2g8e26io41ht.apps.googleusercontent.com';
+$('#contact-form').submit(function(e){
+  e.preventDefault();
 
-      var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+  var $form = $(this);
 
-      function sendmail() {
-        console.log('hello its me');
-        $.ajax({
-            url: '/sendmail.php',
-            type: 'POST'
-        }).done(function(data){
-                alert(JSON.stringify(data));
-        });
-      }
+  var $inputs = $form.find("input, select, button, textarea");
 
-      /**
-       * Check if current user has authorized this application.
-       */
-      function checkAuth() {
-        gapi.auth.authorize(
-          {
-            'client_id': CLIENT_ID,
-            'scope': SCOPES.join(' '),
-            'immediate': true
-          });
-      }
+  var serializedData = $form.serialize();
 
-      /**
-       * Handle response from authorization server.
-       *
-       * @param {Object} authResult Authorization result.
-       */
-      function handleAuthResult(authResult) {
-        var authorizeDiv = document.getElementById('authorize-div');
-        if (authResult && !authResult.error) {
-          // Hide auth UI, then load client library.
-          authorizeDiv.style.display = 'none';
-          loadGmailApi();
-        } else {
-          // Show auth UI, allowing the user to initiate authorization by
-          // clicking authorize button.
-          authorizeDiv.style.display = 'inline';
-        }
-      }
+  $inputs.prop("disabled", true);
 
-      /**
-       * Initiate auth flow in response to user clicking authorize button.
-       *
-       * @param {Event} event Button click event.
-       */
-      function handleAuthClick(event) {
-        gapi.auth.authorize(
-          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-          handleAuthResult);
-        return false;
-      }
+  $.ajax({
+    url: '/sendmail.php',
+    type: 'POST',
+    data: serializedData
+  }).done(function(data){
+    alert('Tu mensaje ha sido enviado con exito');
+  }).always(function(data){
+    $inputs.prop("disabled", false);
+  })
 
-      /**
-       * Load Gmail API client library. List labels once client library
-       * is loaded.
-       */
-      function loadGmailApi() {
-        gapi.client.load('gmail', 'v1', listLabels);
-      }
-
-      /**
-       * Sends an email to the Biomekatronica email address using
-       * the credentials of the authenticated user.
-       */
-
-      function sendMessage(userId, email, callback) {
-        var base64EncodedEmail = btoa(email);
-        var request = gapi.client.gmail.users.messages.send({
-          'userId': 'me',
-          'message': {
-            'raw': base64EncodedEmail
-          }
-        });
-        request.execute(callback);
-      }
-
-
-      /**
-       * Print all Labels in the authorized user's inbox. If no labels
-       * are found an appropriate message is printed.
-       */
-      function listLabels() {
-        var request = gapi.client.gmail.users.labels.list({
-          'userId': 'me'
-        });
-
-        request.execute(function(resp) {
-          var labels = resp.labels;
-          appendPre('Labels:');
-
-          if (labels && labels.length > 0) {
-            for (i = 0; i < labels.length; i++) {
-              var label = labels[i];
-              appendPre(label.name)
-            }
-          } else {
-            appendPre('No Labels found.');
-          }
-        });
-      }
-
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
-      function appendPre(message) {
-        var pre = document.getElementById('output');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-      }
+})
